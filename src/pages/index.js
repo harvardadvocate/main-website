013f6ae273df0c0da3eefed09faa216be48622de
Serena Davis <serenajdavis@gmail.com>
2020-03-02 16:32:55 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -149,7 +149,6 @@ export default ({data}) => (
                             </a>
                     </article>
                 </div>
-
                 <div class="col-md-4">
                     <article>
                         <a href="/advertise" class="sponsored-link"> Sponsored </a>
