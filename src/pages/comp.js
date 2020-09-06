97b3e47d5148c4aebfcee9bca8da4e5569ce5209
Serena Davis <serenajdavis@gmail.com>
2020-09-06 11:37:03 -0700

    Update comp.js
    Added hyperlink

--- src/pages/comp.js
+++ src/pages/comp.js
@@ -16,7 +16,7 @@ export default () => (
                 Fall 2020 comp. We feel that holding a comp remotely at present would pose issues of accessibility, due to varying degrees of access
                 to digital resources, while preventing the magazine from welcoming new members to our community in a meaningful and substantive way.
                 In lieu of a comp, we will be hosting open events, workshops, and projects — all of which will provide opportunities for new students
-                to become involved in Advocate life. Please fill out the form linked here if you would like to be kept in the loop about ways to participate
+                to become involved in Advocate life. Please fill out the form linked <a href="url">https://forms.gle/9yopJaYJ8XToTbDc9</a> if you would like to be kept in the loop about ways to participate
                 in Advocate activities this semester. We anticipate returning to our comp cycle in Spring 2021, with a redesigned comp that responds to the
                 structure of the spring term.
             </p>
