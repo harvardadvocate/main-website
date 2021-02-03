7c6eb023947e2fa19981e00e52c6fdc084e69f6b
Andy Wang <azwang@college.harvard.edu>
2021-02-02 22:42:26 -0500

    fixed slashed

--- src/pages/comp.js
+++ src/pages/comp.js
@@ -13,15 +13,15 @@ export default () => (
             <p></p>
             <p class="text">
    The Harvard Advocate has been a home for arts and literature at Harvard since 1866, and we're looking forward to meeting new members through our virtual Spring 2021 comp! Across its seven boards, The Advocate welcomes critics, artists, writers, designers, programmers, and dealmakers alike. No previous experience is necessary. You don’t have to be a writer or an artist—as long as you’re interested in what we do, Advo has a place for you. 
-   The Advocate will be holding comps for all seven boards: Art, Features, Fiction, Poetry, Design, Business, and Tech. To learn more about the organization, our boards, and the comp process, visit our <a href = "https://www.instagram.com/harvardadvocate/"> Instagram <\a>and<a href = "https://docs.google.com/forms/d/e/1FAIpQLSdBCQ_0og9oqY2kJ8OzZfINn181lUFv2ushPJamHocHl4R9aQ/viewform"> sign up<\a> for one of our intro comp meetings on Zoom--
+   The Advocate will be holding comps for all seven boards: Art, Features, Fiction, Poetry, Design, Business, and Tech. To learn more about the organization, our boards, and the comp process, visit our <a href = "https://www.instagram.com/harvardadvocate/"> Instagram </a>and<a href = "https://docs.google.com/forms/d/e/1FAIpQLSdBCQ_0og9oqY2kJ8OzZfINn181lUFv2ushPJamHocHl4R9aQ/viewform"> sign up</a> for one of our intro comp meetings on Zoom--
    <ul>
-      <li>THURS, FEB. 4/9PM EST<\li>
-      <li>SAT, FEB. 6/3PM EST<\li>
-   <\ul>
+      <li>THURS, FEB. 4/9PM EST</li>
+      <li>SAT, FEB. 6/3PM EST</li>
+   </ul>
 
-   Register and get the link on <a href = "https://docs.google.com/forms/d/e/1FAIpQLSdBCQ_0og9oqY2kJ8OzZfINn181lUFv2ushPJamHocHl4R9aQ/viewform">this form<\a>.
+   Register and get the link on <a href = "https://docs.google.com/forms/d/e/1FAIpQLSdBCQ_0og9oqY2kJ8OzZfINn181lUFv2ushPJamHocHl4R9aQ/viewform">this form</a>.
 
-   For any questions, comments or concerns, contact <a href = "mailto:dionysi@theharvardadvocate.com"> dionysi@theharvardadvocate.com <\a>.
+   For any questions, comments or concerns, contact <a href = "mailto:dionysi@theharvardadvocate.com"> dionysi@theharvardadvocate.com </a>.
             </p>
             <p></p>
         </div>
